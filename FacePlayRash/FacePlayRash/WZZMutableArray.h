599cc66ba3dcd51acbbc9e0ae759b2fb610dd5c6
13731160065 <714318973@qq.com>
2016-03-09 15:51:58 +0800

    本地数组

--- FacePlayRash/FacePlayRash/WZZMutableArray.h
+++ FacePlayRash/FacePlayRash/WZZMutableArray.h
@@ -7,14 +7,83 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 #import "FMDatabase.h"
 
 @interface WZZMutableArray : NSObject
 
 @property (strong, nonatomic) FMDatabase * fmdb;
 
-+ (instancetype)array;
+/**
+ 初始化
+ */
++ (instancetype)setup;
+
+/**
+ 初始化
+ */
++ (instancetype)shareWZZMutableArray;
+
+/**
+ 创建数组，记得用release释放
+ */
+- (void)arrayWithName:(NSString *)name success:(void(^)())successBlock failed:(void(^)())failedBlock;
+
+/**
+ 添加图片
+ */
+- (void)addImage:(UIImage *)image arrName:(NSString *)name success:(void(^)())successBlock failed:(void(^)())failedBlock;
+
+/**
+ 添加元素❌
+ */
+- (void)addObject:(id)obj arrName:(NSString *)name;
 
 
+/**
+ 添加图片从数组遍历
+ */
+- (void)addImages:(NSMutableArray <UIImage *>*)arr arrName:(NSString *)name;
+
+/**
+ 添加元素从数组遍历❌
+ */
+- (void)addObjects:(NSMutableArray *)arr arrName:(NSString *)name;
+
+
+/**
+ 修改图片
+ */
+- (void)replacementImage:(UIImage *)image atIndex:(NSInteger)index arrName:(NSString *)name success:(void(^)())successBlock failed:(void(^)())failedBlock;
+
+/**
+ 修改元素❌
+ */
+- (void)replacementObject:(UIImage *)obj atIndex:(NSInteger)index arrName:(NSString *)name;
+
+/**
+ 取出图片
+ */
+- (UIImage *)imageWithIndex:(NSInteger)index arrName:(NSString *)name;
+
+/**
+ 取出元素❌
+ */
+- (id)objectWithIndex:(NSInteger)index arrName:(NSString *)name;
+
+/**
+ 删除所有元素
+ */
+- (void)removeAllObjectsWithName:(NSString *)name success:(void(^)())successBlock failed:(void(^)())failedBlock;
+
+/**
+ 释放数组
+ */
+- (void)releaseArrWithName:(NSString *)name;
+
+/**
+ 数组元素数量
+ */
+- (NSInteger)countWithName:(NSString *)name;
 
 @end
--- FacePlayRash/FacePlayRash/WZZMutableArray.m
+++ FacePlayRash/FacePlayRash/WZZMutableArray.m
@@ -10,6 +10,7 @@
 
 @implementation WZZMutableArray
 
+#pragma mark - 单利
 static WZZMutableArray *_instance;
 + (id)allocWithZone:(struct _NSZone *)zone
 {
@@ -21,16 +22,122 @@ static WZZMutableArray *_instance;
     return _instance;
 }
 
-+ (instancetype)array
+#pragma mark - 初始化
+//初始化
++ (instancetype)setup
 {
     if (_instance == nil) {
         _instance = [[WZZMutableArray alloc] init];
-        _instance.fmdb = [[FMDatabase alloc] initWithPath:[NSHomeDirectory() stringByAppendingString:@"/Documents/wzzmutablearr.db"]];
+        _instance.fmdb = [FMDatabase databaseWithPath:[NSHomeDirectory() stringByAppendingString:@"/Documents/wzzmutablearr.db"]];
+        [_instance.fmdb open];
     }
-    
     return _instance;
 }
 
+//初始化
++ (instancetype)shareWZZMutableArray {
+    return [self setup];
+}
 
+#pragma mark - 操作数据
+//添加一个图片
+- (void)addImage:(UIImage *)image arrName:(NSString *)name success:(void(^)())successBlock failed:(void(^)())failedBlock {
+    NSData * data = UIImagePNGRepresentation(image);
+    if ([_fmdb executeUpdate:[NSString stringWithFormat:@"insert into %@(obj) values(?);", name], data]) {
+        //成功
+        NSLog(@"插入成功");
+        if (successBlock) {
+            successBlock();
+        }
+    } else {
+        //失败
+        NSLog(@"插入失败");
+        if (failedBlock) {
+            failedBlock();
+        }
+    }
+}
+
+//删除所有数据
+- (void)removeAllObjectsWithName:(NSString *)name success:(void(^)())successBlock failed:(void(^)())failedBlock {
+    if ([_fmdb executeUpdate:[NSString stringWithFormat:@"delete from %@;", name]]) {
+        if (successBlock) {
+            successBlock();
+        }
+    } else {
+        if (failedBlock) {
+            failedBlock();
+        }
+    }
+}
+
+//修改某张图
+- (void)replacementImage:(UIImage *)image atIndex:(NSInteger)index arrName:(NSString *)name success:(void(^)())successBlock failed:(void(^)())failedBlock {
+    if ([_fmdb executeUpdate:[NSString stringWithFormat:@"update %@ set obj=? where idx=?", name], UIImagePNGRepresentation(image), [NSString stringWithFormat:@"%ld", index+1]]) {
+        //成功
+        if (successBlock) {
+            successBlock();
+        }
+    } else {
+        //失败
+        if (failedBlock) {
+            failedBlock();
+        }
+    }
+}
+
+#pragma mark - 查询表
+//查找某项
+- (UIImage *)imageWithIndex:(NSInteger)index arrName:(NSString *)name {
+    FMResultSet * result = [_fmdb executeQuery:[NSString stringWithFormat:@"select * from %@ where idx=?;", name], [NSString stringWithFormat:@"%ld", index+1]];
+    if ([result next]) {
+        //找到
+        return [UIImage imageWithData:[result dataForColumn:@"obj"]];
+    } else {
+        //没找到
+        return nil;
+    }
+}
+
+//查询数量
+- (NSInteger)countWithName:(NSString *)name {
+    FMResultSet * result = [_fmdb executeQuery:[NSString stringWithFormat:@"select count(*) as countNum from %@;", name]];
+    if ([result next]) {
+        return [result intForColumn:@"countNum"];
+    }
+    return 0;
+}
+
+#pragma mark - 操作表
+//创建表
+- (void)arrayWithName:(NSString *)name success:(void(^)())successBlock failed:(void(^)())failedBlock {
+    if ([_fmdb executeUpdate:[NSString stringWithFormat:@"create table if not exists %@(idx integer primary key, obj blob);", name]]) {
+        //成功
+        NSLog(@"创建表成功");
+        if (successBlock) {
+            successBlock();
+        }
+        
+    } else {
+        //失败
+        NSLog(@"创建表失败");
+        if (failedBlock) {
+            failedBlock();
+        }
+    }
+}
+
+//删除表
+- (void)releaseArrWithName:(NSString *)name success:(void(^)())successBlock failed:(void(^)())failedBlock {
+    if ([_fmdb executeUpdate:[NSString stringWithFormat:@"drop table %@", name]]) {
+        if (successBlock) {
+            successBlock();
+        }
+    } else {
+        if (failedBlock) {
+            failedBlock();
+        }
+    }
+}
 
 @end
