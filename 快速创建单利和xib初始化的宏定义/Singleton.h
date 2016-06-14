e9463044c548b18b296023ab427534d9017c55e4
13731160065 <714318973@qq.com>
2016-06-14 12:00:26 +0800

    ,,,,

--- 快速创建单利和xib初始化的宏定义/Singleton.h
+++ 快速创建单利和xib初始化的宏定义/Singleton.h
@@ -1,8 +1,31 @@
+#pragma mark - @interface
+
+/**
+ 功能:
+ 快速创建单例
+ 使用方法:
+ 1.将 SIN_INTERFACE(类名) 添加到.h中
+ 2.将 SIN_IMPLEMENTATION(类名) 添加到.m中
+ */
+#define singleton_interface(class) SIN_INTERFACE(class)
+#define singleton_implementation(class) SIN_IMPLEMENTATION(class)
+
+/**
+ 重写init方法，让init方法读取nib文件
+ 使用方法:
+ 将 INITXIB(NSString_NibName) 添加到.m中
+ */
+#define INITXIB(NSString_NibName) INITXIB_M(NSString_NibName)
+
+#pragma mark end
+
+#pragma mark - @implementation
+
 // .h
-#define singleton_interface(class) + (instancetype)shared##class;
+#define SIN_INTERFACE(class) + (instancetype)shared##class;
 
 // .m
-#define singleton_implementation(class) \
+#define SIN_IMPLEMENTATION(class) \
 static class *_instance; \
 \
 + (id)allocWithZone:(struct _NSZone *)zone \
@@ -25,7 +48,9 @@ return _instance; \
 }
 
 //.m
-#define INITXIB(NSString_NibName) \
+#define INITXIB_M(NSString_NibName) \
 - (instancetype)init {\
-    return [[NSBundle mainBundle] loadNibNamed:NSString_NibName owner:nil options:nil][0];\
+return [[NSBundle mainBundle] loadNibNamed:NSString_NibName owner:nil options:nil][0];\
 }
+
+#pragma mark end
